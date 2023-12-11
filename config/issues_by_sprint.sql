6b421be2dbcdeb7e11300ab71f3b56bb778a0b0a
Alan Cesar Elias <alancesarelias@gmail.com>
2023-12-11 08:53:27 -0300

    chore: add issues_by_sprint.sql

---
+++ config/issues_by_sprint.sql
@@ -0,0 +1,9 @@
+select issue.*,
+       sprint.id as sprint_id
+from issues issue
+         inner join (select issue_id, MAX(sprint_id) as sprint_id
+                     from issue_sprints
+                     group by issue_id) last_sprint on issue.id = last_sprint.issue_id
+         inner join issue_types issue_type on issue.issue_type_id = issue_type.id
+         inner join sprints sprint on last_sprint.sprint_id = sprint.id
+where issue_type.subtask is false
