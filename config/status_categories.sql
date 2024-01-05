e915de9762d2e97a2d6f576cdd27fb3018db6726
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2024-01-05 18:06:58 -0300

    chore: remove outdated sql files

--- config/status_categories.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.status_categories (id, name) VALUES (1, 'No Category');
-INSERT INTO public.status_categories (id, name) VALUES (2, 'To Do');
-INSERT INTO public.status_categories (id, name) VALUES (3, 'Done');
-INSERT INTO public.status_categories (id, name) VALUES (4, 'In Progress');
