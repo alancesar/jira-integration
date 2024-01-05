5edaa3e5738c7f0aef5190dfe90a84582457b03a
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2024-01-05 18:06:21 -0300

    refactor: update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"jira-integration/internal/database"
 	"jira-integration/pkg/gateway"
@@ -17,85 +18,47 @@ import (
 
 var (
 	full    bool
-	project string
+	query   string
+	boardID int
 )
 
 func init() {
 	flag.BoolVar(&full, "full", false, "run sync for all issues")
-	flag.StringVar(&project, "project", "MAQ", "project key")
+	flag.StringVar(&query, "project", "project = DFX", "project key")
+	flag.IntVar(&boardID, "board-id", 66, "board id")
 	flag.Parse()
 }
 
 func main() {
 	ctx := context.Background()
-	sqliteConnection, err := gorm.Open(postgres.Open("host=localhost user=postgres password=Pa55w0rd dbname=postgres port=5432 sslmode=disable TimeZone=America/Sao_Paulo"), &gorm.Config{})
+
+	gormConnection, err := connectionFactory()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
-	db := database.NewSQLite(sqliteConnection)
-	args := []string{fmt.Sprintf("project = %s", project)}
-	//if !full {
-	//	days := retrieveDaysSinceLastUpdate(ctx, db)
-	//	args = append(args, fmt.Sprintf("updated >= -%dd", days))
-	//}
+	db := database.NewSQLite(gormConnection)
+	args := []string{query}
+	if !full {
+		days := retrieveDaysSinceLastUpdate(ctx, db)
+		args = append(args, fmt.Sprintf("updated >= -%dd", days))
+	}
 
 	credentials := jira.Credentials{
 		Username: os.Getenv("JIRA_USERNAME"),
 		Password: os.Getenv("JIRA_PASSWORD"),
 	}
 
-	client := jira.NewClient(credentials, http.DefaultClient)
+	client := jira.NewClient(boardID, credentials, http.DefaultClient)
+	g := gateway.New(client)
 
-	//issueTypes, err := client.GetIssueTypes()
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//
-	//for _, issueType := range issueTypes {
-	//	fmt.Println("fetching issue type", issueType.Name)
-	//	if err := db.SaveIssueType(ctx, issueType); err != nil {
-	//		log.Fatalln(err)
-	//	}
-	//}
-	//
-	//statuses, err := client.GetStatuses()
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//
-	//for _, status := range statuses {
-	//	fmt.Println("fetching status", status.Name)
-	//	if err := db.SaveStatus(ctx, status); err != nil {
-	//		log.Fatalln(err)
-	//	}
-	//}
-	//
-	boardID := 66
-	sprints := client.StreamSprints(boardID)
-	for sprint := range sprints {
-		fmt.Println("fetching sprint", sprint.Name)
-		if err := db.SaveSprint(ctx, sprint); err != nil {
+	if full {
+		if err := g.SyncDependencies(ctx); err != nil {
 			log.Fatalln(err)
 		}
 	}
-	//
-	//fixVersions := client.StreamFixVersions(boardID)
-	//for fixVersion := range fixVersions {
-	//	fmt.Println("fetching fix version", fixVersion.Name)
-	//	if err := db.SaveFixVersion(ctx, fixVersion); err != nil {
-	//		log.Fatalln(err)
-	//	}
-	//}
 
-	g := gateway.New(client)
-	issues := g.StreamAllIssues(args...)
-	for issue := range issues {
-		fmt.Println("fetching issue", issue.Key)
-		if err := db.SaveIssue(ctx, issue); err != nil {
-			log.Println(err)
-		}
-	}
+	g.SyncIssues(ctx, args...)
 }
 
 func retrieveDaysSinceLastUpdate(ctx context.Context, db *database.SQLite) int {
@@ -105,4 +68,12 @@ func retrieveDaysSinceLastUpdate(ctx context.Context, db *database.SQLite) int {
 	}
 
 	return int(time.Since(lastUpdate).Hours()/24) + 1
+}
+
+func connectionFactory() (*gorm.DB, error) {
+	if dsn := os.Getenv("POSTGRES_DSN"); dsn == "" {
+		return gorm.Open(sqlite.Open("sqlite.db"), &gorm.Config{})
+	} else {
+		return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	}
 }
