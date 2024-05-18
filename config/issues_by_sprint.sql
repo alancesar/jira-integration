27a34f98676025c035264df18c7a483b6e937a37
Alan Elias <alan.elias@br.ebury.com>
2024-05-18 09:33:47 -0300

    chore: update issues_by_sprint.sql

--- config/issues_by_sprint.sql
+++ config/issues_by_sprint.sql
@@ -1,11 +1,15 @@
 create view if not exists issues_by_sprint as
 select issue.*,
+       issue_type.id as issue_type_id,
        issue_type.name as issue_type_name,
-       sprint.id as sprint_id
+       issue_type.subtask as is_subtask,
+       sprint.id as sprint_id,
+       sprint.name as sprint_name,
+       sprint.state as sprint_state,
+       sprint.completed_at as sprint_completed_at
 from issues issue
          inner join (select issue_id, MAX(sprint_id) as sprint_id
                      from issue_sprints
                      group by issue_id) last_sprint on issue.id = last_sprint.issue_id
          inner join issue_types issue_type on issue.issue_type_id = issue_type.id
          inner join sprints sprint on last_sprint.sprint_id = sprint.id
-where issue_type.subtask is false
