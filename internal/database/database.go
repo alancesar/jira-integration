297f7a1942aa6c8449a33d90b41d2333b2b90b26
Alan Elias <alan.elias@br.ebury.com>
2024-05-27 16:58:03 -0300

    refactor: remove create view command

--- internal/database/database.go
+++ internal/database/database.go
@@ -30,20 +30,6 @@ func NewSQLite(db *gorm.DB) *SQLite {
 		log.Fatal("while running auto migrate", err)
 	}
 
-	if err := db.Raw(`
-		create view if not exists issues_by_sprint as
-		select issue.*,
-			   sprint.id as sprint_id
-		from issues issue
-				 inner join (select issue_id, MAX(sprint_id) as sprint_id
-							 from issue_sprints
-							 group by issue_id) last_sprint on issue.id = last_sprint.issue_id
-				 inner join issue_types issue_type on issue.issue_type_id = issue_type.id
-				 inner join sprints sprint on last_sprint.sprint_id = sprint.id
-		where issue_type.subtask is false`).Error; err != nil {
-		log.Fatalln("wile creating 'issues_by_sprint' view", err)
-	}
-
 	return &SQLite{
 		db: db,
 	}
