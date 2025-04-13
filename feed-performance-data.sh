b229d63ceb11f5d1429da1d0ff392f481ec85467
Alan Elias <alan.elias@ebury.com>
2025-04-13 11:49:40 -0300

    chore: fetch epics before tasks

--- feed-performance-data.sh
+++ feed-performance-data.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype NOT IN (subTaskIssueTypes(), Theme, \"Sprint Config\") AND updated >= -10d"
+./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype IN (Epic) AND updated >= -10d"
+./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype NOT IN (subTaskIssueTypes(), Theme, Epic, \"Sprint Config\") AND updated >= -10d"
