51568bb6ceb6467b557d734710755dd626c917dd
Alan Elias <alan.elias@ebury.com>
2025-05-12 10:40:52 -0300

    chore: add sync command to the feed-performance-data.sh script

--- feed-performance-data.sh
+++ feed-performance-data.sh
@@ -2,3 +2,4 @@
 
 ./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype IN (Epic) AND updated >= -10d"
 ./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype NOT IN (subTaskIssueTypes(), Theme, Epic, \"Sprint Config\") AND updated >= -10d"
+./bin/sync active future
