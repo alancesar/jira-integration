e46ff914c453e186ea12e36953355e7c9b7d2798
Alan Elias <alan.elias@ebury.com>
2025-06-09 19:45:49 -0300

    chore: use variable in Theme issues query

--- feed-performance-data.sh
+++ feed-performance-data.sh
@@ -2,7 +2,7 @@
 
 range="-15d"
 
-./bin/fetch --jql="issuetype IN (Theme) AND updated >= -35d"
+./bin/fetch --jql="issuetype IN (Theme) AND updated >= "${range}
 ./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype IN (Epic)  AND updated >= "${range}
 ./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype NOT IN (subTaskIssueTypes(), Theme, Epic, \"Sprint Config\") AND updated >= "${range}
 ./bin/sync active future
