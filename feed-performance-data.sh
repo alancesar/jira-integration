961f421dc6ed7d9db17987a35e5c9113a0d91680
Alan Cesar Elias <alan.elias@ebury.com>
2025-08-12 10:29:37 -0300

    chore: add new projects

--- feed-performance-data.sh
+++ feed-performance-data.sh
@@ -3,6 +3,6 @@
 range="-15d"
 
 ./bin/fetch --jql="issuetype IN (Theme) AND updated >= "${range}
-./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype IN (Epic)  AND updated >= "${range}
+./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\", \"One-to-One FX\", \"Developer Experience\", \"Ebury Now\") AND issuetype IN (Epic)  AND updated >= "${range}
 ./bin/fetch --jql="project IN (\"Digital FX\", \"FX Core\") AND issuetype NOT IN (subTaskIssueTypes(), Theme, Epic, \"Sprint Config\") AND updated >= "${range}
 ./bin/sync active future
